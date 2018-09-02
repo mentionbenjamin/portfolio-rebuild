88866faf79b481777cba199b215ac23b25eead5a
Ben Robinson <mentionbenjamin@gmail.com>
2018-09-02 14:17:54 +0100

    adds SidePanelContainer and Header to get the ball rolling

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div className="side-panel-header">
+      <h1>
+        Header Placeholder
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
