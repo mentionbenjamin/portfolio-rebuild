82e292ee46c4208c9920992953e44cca791f5a64
Ben Robinson <mentionbenjamin@gmail.com>
2018-09-04 17:59:37 +0100

    adds link span to each codeclan project for heroku links later... signing out for the day now

--- src/components/data/HomeData.js
+++ src/components/data/HomeData.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const HomeData = {
   name: 'Home',
-  header: (<h1>The aim is to <span>cool,</span> wow and move people through digital experiences.</h1>),
+  header: (<h1 className="header-text">The aim is to <span className="cool"><a href="https://www.ted.com/talks/elora_hardy_magical_houses_made_of_bamboo/up-next?utm_campaign=social&utm_medium=referral&utm_source=facebook.com&utm_content=talk&utm_term=art-design" rel="noopener noreferrer" target="_blank">cool,</a></span> wow and move people through digital experiences.</h1>),
   details: '',
-  description: (<h3>Working between design & development, I find nothing more rewarding than cracking a clients puzzle in an inventive, effective way.<br/><br/>I am looking for an opportunity in an ambitious, lateral-thinking environment where I can grow and develop. Helping add value by building engaging digital solutions.<br/><br/>As with all projects, there were limitations, commiserations and celebrations, so hit me up to learn the story behind the thinking.</h3>),
+  description: (<h3>Working between design & development, I find nothing more rewarding than cracking a clients puzzle in an inventive, effective way.<br/><br/>I am looking for an opportunity in an ambitious, lateral-thinking environment where I can grow and develop; helping add value by building engaging digital solutions.<br/><br/>As with all projects, there were limitations, commiserations and celebrations—so hit me up to learn more about the story behind each project.</h3>),
   footer: (<h4>mentionbenjamin</h4>)
 }
 
