77a24c02fd69b9f81b7d71e599f76d13b87fe100
Ben Robinson <mentionbenjamin@gmail.com>
2018-09-04 16:58:34 +0100

    adds span classes making bold for tools used, time-frame, environment

--- src/components/data/HomeData.js
+++ src/components/data/HomeData.js
@@ -5,7 +5,7 @@ const HomeData = {
   name: 'Home',
   header: (<h1>The aim is to <span>cool,</span> wow and move people through digital experiences.</h1>),
   details: '',
-  description: (<h3>Working between design & development, I find nothing more rewarding than cracking a clients puzzle in an inventive, effective way.<br/><br/>I am looking for an opportunity in an ambitious, lateral-thinking environment where I can grow and develop. Helping add value by building engaging digital solutions.</h3>),
+  description: (<h3>Working between design & development, I find nothing more rewarding than cracking a clients puzzle in an inventive, effective way.<br/><br/>I am looking for an opportunity in an ambitious, lateral-thinking environment where I can grow and develop. Helping add value by building engaging digital solutions.<br/><br/>As with all projects, there were limitations, commiserations and celebrations, so hit me up to learn the story behind the thinking.</h3>),
   footer: (<h4>mentionbenjamin</h4>)
 }
 
--- src/components/data/TrackYoselfData.js
+++ src/components/data/TrackYoselfData.js
@@ -5,7 +5,7 @@ const TrackYoselfData = {
   name: 'TrackYoself',
   header: (<h1>Track Yo'self</h1>),
   caption: (<h3>Spending tracker web application with an animated UI.</h3>),
-  details: (<h3>Time-frame: 6 days<br/>Environment: CodeClan Bootcamp<br/>Tools: Ruby, Sinatra, HTML5, CSS3, pSQL</h3>),
+  details: (<h3><span className="details-bold">Time-frame:</span> 6 days<br/><span className="details-bold">Environment:</span> CodeClan Bootcamp<br/><span className="details-bold">Tools:</span> Ruby, Sinatra, HTML5, CSS3, pSQL</h3>),
   description: (<h3>Tamen imitarentur singulis tamen appellat e aut veniam summis dolor laborum, appellat exquisitaque, nisi ab quo aute cernantur.Nostrud firmissimum qui vidisse qui aut irure transferrem, aliquip e duis iis aliqua mandaremus excepteur.<br/><br/>Quo esse relinqueret.Meiusmod, dolor pariatur de singulis. Ex officia cohaerescant, duis consectetur probant elit litteris. Excepteur minim nescius laboris quo nescius si veniam.</h3>),
   footer: (<h4>mentionbenjamin</h4>)
 }
